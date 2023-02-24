66e490c06395c47002e994d5e80639e8ece4a41f
Michael Renzo Diaz <mrenzodiaz@gmail.com>
2023-02-24 09:58:23 +0800

    reexported images under about me page

--- src/constants/about-me.js
+++ src/constants/about-me.js
@@ -3,37 +3,37 @@ export const aboutMeCareerJourney = [
     name: 'NCCER',
     position: 'Lead UX/UI Designer',
     duration: 'June 2021 - Present',
-    imgUrl: 'images/NCCER.svg',
+    imgUrl: 'images/NCCER.png',
   },
   {
     name: 'ToothFairy',
     position: 'UI Designer',
     duration: 'April 2020 - May 2022',
-    imgUrl: 'images/TF.svg',
+    imgUrl: 'images/TF.png',
   },
   {
     name: 'POPflow',
     position: 'UX/UI Designer',
     duration: 'November 2020 - August 2021',
-    imgUrl: 'images/PopFlow.svg',
+    imgUrl: 'images/PopFlow.png',
   },
   {
     name: 'Geekbears',
     position: 'UX/UI Designer',
     duration: 'October 2019 - April 2020',
-    imgUrl: 'images/Geekbears.svg',
+    imgUrl: 'images/Geekbears.png',
   },
   {
     name: 'CrowdComfort',
     position: 'UI Designer',
     duration: 'October 2018 - February 2021',
-    imgUrl: 'images/CrowdComfort.svg',
+    imgUrl: 'images/CrowdComfort.png',
   },
 ];
 
 export const aboutMeToolbox = [
   {
-    imgUrl: 'images/Discovery.svg',
+    imgUrl: 'images/Discovery.png',
     customContent: [
       'Stakeholder Interviews',
       'User Interviews',
@@ -43,7 +43,7 @@ export const aboutMeToolbox = [
     ],
   },
   {
-    imgUrl: 'images/Explore.svg',
+    imgUrl: 'images/Explore.png',
     customContent: [
       'Design Review',
       'Competitive Analysis',
@@ -54,11 +54,11 @@ export const aboutMeToolbox = [
     ],
   },
   {
-    imgUrl: 'images/Listen.svg',
+    imgUrl: 'images/Listen.png',
     customContent: ['Surveys', 'Analytics Review', 'Feedback Review'],
   },
   {
-    imgUrl: 'images/Define.svg',
+    imgUrl: 'images/Define.png',
     customContent: [
       'User Stories and Mapping',
       'Information Architecture',
@@ -67,7 +67,7 @@ export const aboutMeToolbox = [
     ],
   },
   {
-    imgUrl: 'images/Designs.svg',
+    imgUrl: 'images/Designs.png',
     customContent: [
       'Sketches',
       'Wireframes',
@@ -77,7 +77,7 @@ export const aboutMeToolbox = [
     ],
   },
   {
-    imgUrl: 'images/Prototype.svg',
+    imgUrl: 'images/Prototype.png',
     customContent: [
       'Clickable Prototype',
       'Interaction Prototype (Protopie)',
@@ -92,30 +92,30 @@ export const aboutMeHobbies = [
     name: 'Blender',
     position: 'Lvl 1 - Making 3D Donuts',
     duration: 'January 2022 - Present',
-    imgUrl: 'images/Blender.svg',
+    imgUrl: 'images/Blender.png',
   },
   {
     name: 'Tennis',
     position: 'Lvl 3.3 - Currently Seeking a Tennis Team',
     duration: 'May 2021 - Present',
-    imgUrl: 'images/Tennis.svg',
+    imgUrl: 'images/Tennis.png',
   },
   {
     name: 'Japanese',
     position: 'Lvl 4 - Reading, Writing, Speaking',
     duration: 'April 2020 - Present',
-    imgUrl: 'images/Japan.svg',
+    imgUrl: 'images/Japan.png',
   },
   {
     name: 'Travel',
     position: 'Lvl 10 - Favorite Country to Visit: Japan',
     duration: 'January 2012 - Present',
-    imgUrl: 'images/Travel.svg',
+    imgUrl: 'images/Travel.png',
   },
   {
     name: 'Visual and Verbal Storytelling',
     position: 'Lvl 2 - Currently In-Progress',
     duration: 'April 2020 - Present',
-    imgUrl: 'images/Storytelling.svg',
+    imgUrl: 'images/Storytelling.png',
   },
 ];
